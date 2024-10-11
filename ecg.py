698e5b8fff2a6e4547a0b97c5d85f3a668a7a9d4
vasanth2703 <avk07373@gmail.com>
2024-10-11 23:47:11 +0530

    Update ecg.py

--- ecg.py
+++ ecg.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-from scikit-learn.preprocessing import StandardScaler
+from scikit.preprocessing import StandardScaler
 import pandas as pd
 
 class ECGModel(nn.Module):
